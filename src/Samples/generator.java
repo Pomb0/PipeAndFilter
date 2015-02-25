eab695e6e0793c63943d1c40b272b6de61f00672
Jaime Live <jaime@slimecraft.pt>
2015-02-25 00:30:32 +0000

    Rearranged The Code.

--- src/Samples/generator.java
+++ src/Samples/generator.java
@@ -8,13 +8,13 @@ import java.util.Random;
 
 
 public class generator {
-	static final int timeid=0;
-	static final int vid=1;
-	static final int aid=2;
-	static final int pid=3;
-	static final int tid=4;
+	static final int timeid = 0;
+	static final int vid = 1;
+	static final int aid = 2;
+	static final int pid = 3;
+	static final int tid = 4;
 
-	static void record(DataOutputStream f, double v, double a, double p, double t) throws IOException{
+	static void record(DataOutputStream f, double v, double a, double p, double t) throws IOException {
 		f.writeInt(timeid);
 		f.writeLong(System.currentTimeMillis());
 		f.writeInt(vid);
@@ -27,23 +27,22 @@ public class generator {
 		f.writeDouble(t);
 	}
 
-	public static void main(String argv[])
-	{
+	public static void main(String argv[]) {
 
 		String fileName = "NewData.dat";
 		DataOutputStream file = null;
-		double v,a,p,t;
+		double v, a, p, t;
 		Random x = new Random(System.currentTimeMillis());
 
 		try {
 			file = new DataOutputStream(new FileOutputStream(fileName));
-			
+
 			// change the constants in the for loop, to generate values in the ranges you want
-			for (int i=1; i<100;i++){
-				v= 250 + 50*x.nextDouble();
-				a= 10000 + 200*x.nextDouble();
-				p= 40 + 60*x.nextDouble();
-				t= -50 + 100*x.nextDouble();
+			for (int i = 1; i < 100; i++) {
+				v = 250 + 50 * x.nextDouble();
+				a = 10000 + 200 * x.nextDouble();
+				p = 40 + 60 * x.nextDouble();
+				t = -50 + 100 * x.nextDouble();
 				record(file, v, a, p, t);
 			}
 			file.close();
--- src/Templates/PlumberTemplate.java
+++ src/Templates/PlumberTemplate.java
@@ -1,51 +1,51 @@
 package Templates;
 
-/******************************************************************************************************************
-* File:PlumberTemplate.java
-* Course: 17655
-* Project: Assignment 1
-* Copyright: Copyright (c) 2003 Carnegie Mellon University
-* Versions:
-*	1.0 November 2008 - Initial rewrite of original assignment 1 (ajl).
-*
-* Description:
-*
-* This class serves as a template for creating a main thread that will instantiate and connect a set of filters.
-* The details of the filter operations is totally self contained within the filters, the "plumber" takes care of
-* starting the filters and connecting each of the filters together. The details of how to connect filters is taken
-* care of by the FilterFramework. In order to use this template the program should rename the class.
-* The template includes the RunFilter() method which is executed when the filter is started. While simple, there
-* are semantics for instantiating, connecting, and starting the filters:
-*
-*	Step 1: Instantiate the filters as shown in the example below. You should create the filters using the
-*			templates provided, and you must use the FilterFramework as a base class for all filters. Every pipe and
-*			filter network must have a source filter where data originates, and a sink filter where the data flow
-*			terminates.
-*
-*	Step 2: Connect the filters. Start with the sink and work backward to the source. Essentially you are connecting
-*			the	input of each filter to the up-stream filter's output until you get to the source filter. Filter have
-*			a Connect() method which accepts a FilterFramework type. This method connects the calling filter's input
-*			to the passed filter's output. Again the example in the comments below illustrates how this is done.
-*
-*	Step 3: Start the filters using the start() method.
-*
-*	Once the filters are started this main thread dies and the pipe and filter network processes data until there
-* 	is no more data movement from the source. Each filter will shutdown when data is no longer available (provided
-*	you followe the read semantics described in the filter templates).
-*	on their input ports.
-*
-* Parameters: 		None
-*
-* Internal Methods:	None
-*
-******************************************************************************************************************/
-public class PlumberTemplate
-{
-   public static void main( String argv[])
-   {
+/**
+ * ***************************************************************************************************************
+ * File:PlumberTemplate.java
+ * Course: 17655
+ * Project: Assignment 1
+ * Copyright: Copyright (c) 2003 Carnegie Mellon University
+ * Versions:
+ * 1.0 November 2008 - Initial rewrite of original assignment 1 (ajl).
+ * <p/>
+ * Description:
+ * <p/>
+ * This class serves as a template for creating a main thread that will instantiate and connect a set of filters.
+ * The details of the filter operations is totally self contained within the filters, the "plumber" takes care of
+ * starting the filters and connecting each of the filters together. The details of how to connect filters is taken
+ * care of by the FilterFramework. In order to use this template the program should rename the class.
+ * The template includes the RunFilter() method which is executed when the filter is started. While simple, there
+ * are semantics for instantiating, connecting, and starting the filters:
+ * <p/>
+ * Step 1: Instantiate the filters as shown in the example below. You should create the filters using the
+ * templates provided, and you must use the FilterFramework as a base class for all filters. Every pipe and
+ * filter network must have a source filter where data originates, and a sink filter where the data flow
+ * terminates.
+ * <p/>
+ * Step 2: Connect the filters. Start with the sink and work backward to the source. Essentially you are connecting
+ * the	input of each filter to the up-stream filter's output until you get to the source filter. Filter have
+ * a Connect() method which accepts a FilterFramework type. This method connects the calling filter's input
+ * to the passed filter's output. Again the example in the comments below illustrates how this is done.
+ * <p/>
+ * Step 3: Start the filters using the start() method.
+ * <p/>
+ * Once the filters are started this main thread dies and the pipe and filter network processes data until there
+ * is no more data movement from the source. Each filter will shutdown when data is no longer available (provided
+ * you followe the read semantics described in the filter templates).
+ * on their input ports.
+ * <p/>
+ * Parameters: 		None
+ * <p/>
+ * Internal Methods:	None
+ * <p/>
+ * ****************************************************************************************************************
+ */
+public class PlumberTemplate {
+	public static void main(String argv[]) {
 		/****************************************************************************
-		* Here we instantiate three filters.
-		****************************************************************************/
+		 * Here we instantiate three filters.
+		 ****************************************************************************/
 		/*
 
 		SourceFilter Filter1 = new SourceFilter();	// This is a source filter - see SourceFilterTemplate.java
@@ -54,11 +54,11 @@ public class PlumberTemplate
 
 		*/
 		/****************************************************************************
-		* Here we connect the filters starting with the sink filter (Filter 1) which
-		* we connect to Filter2 the middle filter. Then we connect Filter2 to the
-		* source filter (Filter3). You must connect filters starting with the sink
-		* filter and working your way back to the source as shown here.
-		****************************************************************************/
+		 * Here we connect the filters starting with the sink filter (Filter 1) which
+		 * we connect to Filter2 the middle filter. Then we connect Filter2 to the
+		 * source filter (Filter3). You must connect filters starting with the sink
+		 * filter and working your way back to the source as shown here.
+		 ****************************************************************************/
 		/*
 
 		Filter3.Connect(Filter2); // This esstially says, "connect Filter3's input port to Filter2's output port
@@ -66,8 +66,8 @@ public class PlumberTemplate
 
 		*/
 		/****************************************************************************
-		* Here we start the filters up.
-		****************************************************************************/
+		 * Here we start the filters up.
+		 ****************************************************************************/
 		/*
 
 		Filter1.start();
@@ -75,6 +75,6 @@ public class PlumberTemplate
 		Filter3.start();
 
 		*/
-   } // main
+	} // main
 
 } // PlumberTemplate
