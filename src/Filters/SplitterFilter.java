b94852cfe07404d589233affdbbfdc53b1b65ad3
Jaime Live <jaime@slimecraft.pt>
2015-02-25 07:48:57 +0000

    Created Stub for splitter

---
+++ src/Filters/SplitterFilter.java
@@ -0,0 +1,15 @@
+package Filters;
+
+import Framework.ExpandedFilterFramework;
+
+/**
+ * Created by Jaime on 25/02/2015.
+ */
+public class SplitterFilter extends ExpandedFilterFramework{
+
+
+	@Override
+	public void run() {
+
+	}
+}
