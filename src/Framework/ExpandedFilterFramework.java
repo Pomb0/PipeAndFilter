8766dabd8602cb39719ad9894389ec3de6f0549f
Jaime Live <jaime@slimecraft.pt>
2015-02-25 01:19:40 +0000

    Extended the framework.

---
+++ src/Framework/ExpandedFilterFramework.java
@@ -0,0 +1,9 @@
+package Framework;
+
+/**
+ * Created by Jaime on 25/02/2015.
+ */
+public abstract class ExpandedFilterFramework extends FilterFramework{
+	@Override
+	public abstract void run();
+}
