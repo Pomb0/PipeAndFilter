23dfbcc91ad242320f3bad46b589cf94646f3922
Jaime Live <jaime@slimecraft.pt>
2015-02-26 05:31:36 +0000

    Updated Spliter to use the new framework.

--- src/Filters/HeightFilter.java
+++ src/Filters/HeightFilter.java
@@ -1,62 +1,16 @@
 package Filters;
 
 import Framework.ExpandedFilterFramework;
+import Framework.Stream.FrameBean;
 
 public class HeightFilter extends ExpandedFilterFramework {
-    int field = 2;
-    public void run(){
-        int measurementLength = 8;
-        int idLength = 4;
-        byte dataByte = 0;
-        int id;
 
-        long measurement;
-        System.out.println(this.getName() + "::Height Filter Starting ");
-
-        while (true) {
-            id = 0;
-            measurement = 0;
-            try {
-
-                for (int i = 0; i < idLength; i++) {
-                    dataByte = ReadFilterInputPort();
-                    id = id | (dataByte & 0xFF);
-                    if (i != idLength - 1) { id = id << 8; }
-                }
-
-                if(id != field) {
-                    for(int i = 0 ; i < 4 ; i++){ WriteFilterOutputPort((byte)((id >> ((7 - i) * 8)) & 0xff)); }
-                    for (int i = 0; i < measurementLength; i++) {
-                        dataByte = ReadFilterInputPort();
-                        measurement = measurement | (dataByte & 0xFF);
-                        if (i != measurementLength - 1) { measurement = measurement << 8; }
-                        WriteFilterOutputPort(dataByte);
-                    }
-                }
-                else {
-                    for (int i = 0; i < measurementLength; i++) {
-                        dataByte = ReadFilterInputPort();
-                        measurement = measurement | (dataByte & 0xFF);
-                        if (i != measurementLength - 1) { measurement = measurement << 8; }
-                    }
-                    FilterDispatcher(ConvertToMeters(Double.longBitsToDouble(measurement)));
-                }
-            } catch (EndOfStreamException e) {
-                ClosePorts();
-                System.out.println(this.getName() + "::Height Filter Exiting ");
-                break;
-            }
-        }
+    @Override
+    public void filter() throws EndOfStreamException {
+        FrameBean frame = readFrame();
     }
 
-    private void FilterDispatcher(double value) {
-        for(int i = 0 ; i < 4 ; i++){ WriteFilterOutputPort((byte)((field >> ((7 - i) * 8)) & 0xff)); }
-        for(int i = 0; i < 8; i++) {
-            WriteFilterOutputPort((byte)((Double.doubleToLongBits(value) >> ((7 - i) * 8)) & 0xff));
-        }
-    }
-
-    private double ConvertToMeters(double v) {
+    private double FeetToMeters(double v) {
         return v / 3.2808;
     }
 }
--- src/Framework/Stream/AttributeBean.java
+++ src/Framework/Stream/AttributeBean.java
@@ -30,16 +30,19 @@ public class AttributeBean implements Serializable, Cloneable{
         return ByteBuffer.wrap(value).getLong();
     }
 
-	public void setKey(int key) {
+	public AttributeBean setKey(int key) {
         this.key = ByteBuffer.allocate(KEYSIZE).putInt(key).array();
+		return this;
 	}
 
-	public void setValue(double value) {
+	public AttributeBean setValue(double value) {
         this.value = ByteBuffer.allocate(VALUESIZE).putDouble(value).array();
+		return this;
 	}
 
-    public void setValue(long value) {
+    public AttributeBean setValue(long value) {
         this.value = ByteBuffer.allocate(VALUESIZE).putLong(value).array();
+	    return this;
     }
 
 
@@ -48,15 +51,17 @@ public class AttributeBean implements Serializable, Cloneable{
 		return key;
 	}
 
-	public void setKey(byte[] key) {
+	public AttributeBean setKey(byte[] key) {
 		this.key = key.clone();
+		return this;
 	}
 
 	public byte[] getValue() {
 		return value;
 	}
 
-	public void setValue(byte[] value) {
+	public AttributeBean setValue(byte[] value) {
 		this.value = value.clone();
+		return this;
 	}
 }
