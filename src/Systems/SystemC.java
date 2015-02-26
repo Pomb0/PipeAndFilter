1fd7e9c25e99aa9d984fc40fc24a6bbe1a1f924d
Jaime Live <jaime@slimecraft.pt>
2015-02-26 20:35:07 +0000

    Finished SystemC

--- src/Systems/SystemC.java
+++ src/Systems/SystemC.java
@@ -1,7 +1,7 @@
 package Systems;
 
+import Filters.FileSinkFilter;
 import Filters.FileSourceFilter;
-import Filters.RawFileSinkFilter;
 import Filters.ZipperFilter;
 
 public class SystemC {
@@ -18,7 +18,8 @@ public class SystemC {
 		FileSourceFilter fileA = new FileSourceFilter(FilePathA);
 		FileSourceFilter fileB = new FileSourceFilter(FilePathB);
 		ZipperFilter zipper = new ZipperFilter();
-		RawFileSinkFilter sink = new RawFileSinkFilter(FileOutput);
+		//RawFileSinkFilter sink = new RawFileSinkFilter(FileOutput);
+		FileSinkFilter sink = new FileSinkFilter(FileOutput);
 
 		/** Connect Filters.
 		 * FileA ---\
