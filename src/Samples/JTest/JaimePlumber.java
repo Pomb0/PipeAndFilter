620776e5ce78f6f268f2c93b1a0567b43282cc88
Jaime Live <jaime@slimecraft.pt>
2015-02-26 18:52:39 +0000

    Finished first version of Zipper, not sufficiently tested yet!

--- src/Samples/JTest/JaimePlumber.java
+++ src/Samples/JTest/JaimePlumber.java
@@ -1,7 +1,27 @@
 package Samples.JTest;
 
+import Filters.FileSinkFilter;
+import Filters.FileSourceFilter;
+import Filters.ZipperFilter;
+
 public class JaimePlumber {
 	public static void main(String argv[]) {
+
+		FileSourceFilter fileA = new FileSourceFilter("data/SubSetA.dat");
+		FileSourceFilter fileB = new FileSourceFilter("data/SubSetB.dat");
+		ZipperFilter zipper = new ZipperFilter();
+		FileSinkFilter sink = new FileSinkFilter("data/AB.dat");
+
+		sink.Connect(zipper);
+		zipper.Connect(fileA);
+		zipper.Connect(fileB);
+
+
+		fileA.start();
+		fileB.start();
+		zipper.start();
+		sink.start();
+
 
 
 	} // main
