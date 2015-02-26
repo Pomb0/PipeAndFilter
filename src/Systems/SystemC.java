34f4164217981dab18c4cedebb1f05dbcfae7d83
Jaime Live <jaime@slimecraft.pt>
2015-02-26 19:26:20 +0000

    Added comment to WilldPointFilter

--- src/Systems/SystemC.java
+++ src/Systems/SystemC.java
@@ -1,4 +1,14 @@
 package Systems;
 
+import Filters.FileSourceFilter;
+
 public class SystemC {
+	public static void main(String argv[]) {
+		/**
+		 * System C gets two subsets of data, A and B, from files, and merges them together
+		 * keeping chronological order.
+		 */
+
+		FileSourceFilter fileA = new FileSourceFilter("data/SubSetA.dat");
+	}
 }
