24aa341ae5ab33741958e9945c4b5ce11abf214f
Jaime Live <jaime@slimecraft.pt>
2015-02-26 20:01:40 +0000

    Finished SystemC

--- src/Systems/SystemC.java
+++ src/Systems/SystemC.java
@@ -1,23 +1,38 @@
 package Systems;
 
 import Filters.FileSourceFilter;
+import Filters.RawFileSinkFilter;
 import Filters.ZipperFilter;
 
 public class SystemC {
 	public static final String FilePathA = "data/SubSetA.dat";
 	public static final String FilePathB = "data/SubSetB.dat";
-	public static final String FileOutput = "data/";
+	public static final String FileOutput = "data/StreamC.dat";
 
 	public static void main(String argv[]) {
 		/**
 		 * System C gets two subsets of data, A and B, from files, and merges them together
 		 * keeping chronological order.
 		 */
-
+		//Create Filters.
 		FileSourceFilter fileA = new FileSourceFilter(FilePathA);
 		FileSourceFilter fileB = new FileSourceFilter(FilePathB);
 		ZipperFilter zipper = new ZipperFilter();
+		RawFileSinkFilter sink = new RawFileSinkFilter(FileOutput);
 
+		/** Connect Filters.
+		 * FileA ---\
+		 *            > Zipper ---> Sink
+		 * FileB ---/
+		 */
+		sink.Connect(zipper);
+		zipper.Connect(fileA);
+		zipper.Connect(fileB);
 
+		//Start Filters.
+		fileA.start();
+		fileB.start();
+		zipper.start();
+		sink.start();
 	}
 }
