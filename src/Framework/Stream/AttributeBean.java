fef61bddf7714e78b62ec1337ce3c46f8913db81
Jaime Live <jaime@slimecraft.pt>
2015-02-26 00:36:21 +0000

    Created AttributeBean

---
+++ src/Framework/Stream/AttributeBean.java
@@ -0,0 +1,50 @@
+package Framework.Stream;
+
+import java.io.Serializable;
+
+/**
+ * Created by Jaime on 26/02/2015.
+ */
+public class AttributeBean implements Serializable{
+	private byte[] key = new byte[4];
+	private byte[] value = new byte[8];
+
+
+	
+	public int getKeyAsInt(){
+		//TODO costa - returns key as an integer
+		return 0;
+	}
+
+	public double getValueAsDouble(){
+		//TODO costa - returns value as a double
+		return 0;
+	}
+
+	public void setKey(int key) {
+		//TODO costa - convert int key to byte array this.key
+	}
+
+
+	public void setValue(double value) {
+		//TODO costa - convert double value to byte array this.value
+	}
+
+
+
+	public byte[] getKey() {
+		return key;
+	}
+
+	public void setKey(byte[] key) {
+		this.key = key;
+	}
+
+	public byte[] getValue() {
+		return value;
+	}
+
+	public void setValue(byte[] value) {
+		this.value = value;
+	}
+}
---
+++ src/Framework/Stream/FrameBean.java
@@ -0,0 +1,9 @@
+package Framework.Stream;
+
+import java.io.Serializable;
+
+/**
+ * Created by Jaime on 26/02/2015.
+ */
+public class FrameBean implements Serializable{
+}
